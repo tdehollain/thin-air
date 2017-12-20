b960c3799e55911b14b4dfae90963473ba5c7a67
Thibaut de Hollain <tdehollain@Thibauts-MBP.lan>
2017-12-21 00:39:22 +0100

    added crontab list

---
+++ crontab-list.sh
@@ -0,0 +1,24 @@
+*/15 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XXBTZEUR 15 >> /home/ubuntu/apps/thin-air/fetchLogs/XXBTZEUR_15.log
+1 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XXBTZEUR 60 >> /home/ubuntu/apps/thin-air/fetchLogs/XXBTZEUR_60.log
+2 */4 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XXBTZEUR 240 >> /home/ubuntu/apps/thin-air/fetchLogs/XXBTZEUR_240.log
+3 7 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XXBTZEUR 1440 >> /home/ubuntu/apps/thin-air/fetchLogs/XXBTZEUR_1440.log
+
+*/15 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XETHZEUR 15 >> /home/ubuntu/apps/thin-air/fetchLogs/XETHZEUR_15.log
+6 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XETHZEUR 60 >> /home/ubuntu/apps/thin-air/fetchLogs/XETHZEUR_60.log
+7 */4 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XETHZEUR 240 >> /home/ubuntu/apps/thin-air/fetchLogs/XETHZEUR_240.log
+8 7 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XETHZEUR 1440 >> /home/ubuntu/apps/thin-air/fetchLogs/XETHZEUR_1440.log
+
+*/15 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XETHXXBT 15 >> /home/ubuntu/apps/thin-air/fetchLogs/XETHXXBT_15.log
+11 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XETHXXBT 60 >> /home/ubuntu/apps/thin-air/fetchLogs/XETHXXBT_60.log
+12 */4 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XETHXXBT 240 >> /home/ubuntu/apps/thin-air/fetchLogs/XETHXXBT_240.log
+13 7 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XETHXXBT 1440 >> /home/ubuntu/apps/thin-air/fetchLogs/XETHXXBT_1440.log
+
+*/15 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XLTCZEUR 15 >> /home/ubuntu/apps/thin-air/fetchLogs/XLTCZEUR_15.log
+16 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XLTCZEUR 60 >> /home/ubuntu/apps/thin-air/fetchLogs/XLTCZEUR_60.log
+17 */4 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XLTCZEUR 240 >> /home/ubuntu/apps/thin-air/fetchLogs/XLTCZEUR_240.log
+18 7 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XLTCZEUR 1440 >> /home/ubuntu/apps/thin-air/fetchLogs/XLTCZEUR_1440.log
+
+*/15 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XLTCXXBT 15 >> /home/ubuntu/apps/thin-air/fetchLogs/XLTCXXBT_15.log
+21 * * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XLTCXXBT 60 >> /home/ubuntu/apps/thin-air/fetchLogs/XLTCXXBT_60.log
+22 */4 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XLTCXXBT 240 >> /home/ubuntu/apps/thin-air/fetchLogs/XLTCXXBT_240.log
+23 7 * * * /home/ubuntu/.nvm/versions/node/v9.3.0/bin/node /home/ubuntu/apps/thin-air/fetchTradeData.js XLTCXXBT 1440 >> /home/ubuntu/apps/thin-air/fetchLogs/XLTCXXBT_1440.log
