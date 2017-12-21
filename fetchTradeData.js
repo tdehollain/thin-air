33fd6311c39c4636050344ed882f9da897f4877e
Thibaut de Hollain <tdehollain@Thibauts-MBP.lan>
2017-12-22 00:02:57 +0100

    added logger

--- fetchTradeData.js
+++ fetchTradeData.js
@@ -1,12 +1,10 @@
-const key          = '873OI0uQRcg62yrA1Fa9pydXV/6n0bBgQnPjvrQnQKpXwf9GNzb8BkbE'; // API Key
-const secret       = 'ZEsrgGWxXYUCVaJrOnh+np82tVFsnc9SSu7RFJTvtZqXmJ19PBcIhgL+erwMPPLEGm8Ha9nD+XRf649AZrTD+w=='; // API Private Key
-const KrakenClient = require('kraken-api');
-const kraken       = new KrakenClient(key, secret);
 
+const lg = require('./lib/logger.js').lg;
 const request = require('request');
+const kraken = require('./lib/krakenCred').kraken;
 const tradeDataModel = require('./lib/db.js').tradeDataModel;
-
 const Util = require('./lib/utils.js');
+
 
 const pair = process.argv[2]; // e.g. ETHZUSD
 const interval = process.argv[3]; // e.g. 1440 = daily
@@ -26,11 +24,11 @@ async function fetchData() {
     } catch(err) {
         APIcalls++;
         if(APIcalls <= maxAPICalls) {
-            console.log(`${err}. Trying again...(call #${APIcalls})`);
+            lg.warn(`${err}. Trying again...(call #${APIcalls})`);
             fetchData();
         } else {
-            console.log(err);
-            console.log("Error: couldn't get a response from Kraken API despite " + (APIcalls-1) + " calls.");
+            lg.info(err);
+            lg.error(new Error("Error: couldn't get a response from Kraken API despite " + (APIcalls-1) + " calls."));
             process.exit(0);
         }
         return;
@@ -43,7 +41,7 @@ async function fetchData() {
         // Check if the record is already in DB
         tradeDataModel.find( { pair: pair, interval: interval, time: time }, (err, docs) => {
             if(docs.length) {
-                console.log(`error: record already present for ${pair}, int. ${interval} min. Time: ${docs[0].time}, value (close): ${docs[0].close}`);
+                lg.warn(`record already present for ${pair}, interval ${interval}min. Time: ${docs[0].time}, value (close): ${docs[0].close}`);
                 recordsProcessed++;
             } else {
                 let newValue = new tradeDataModel({
@@ -60,9 +58,9 @@ async function fetchData() {
                 });
                 newValue.save(err => {
                     if(err) {
-                        console.log(err);
+                        lg.err(err);
                     } else {
-                        console.log(`saved new record: [pair: ${pair}, interval: ${interval}, time: ${Util.timeToDate(time)}, close:${close}]`)
+                        lg.info(`saved new record: [pair: ${pair}, interval: ${interval}, time: ${Util.timeToDate(time)}, close:${close}]`)
                     }
                     recordsProcessed++;
                 });
